3071ccceaac6e00ed5a98525269bf38380b88eb0
Danilo Candido <kodalen@lyune.com>
2021-07-28 13:04:08 -0300

    change window size

--- index.js
+++ index.js
@@ -170,8 +170,8 @@ let win
 function createWindow() {
 
     win = new BrowserWindow({
-        width: 980,
-        height: 552,
+        width: 1100,
+        height: 600,
         icon: getPlatformIcon('SealCircle'),
         frame: false,
         webPreferences: {
